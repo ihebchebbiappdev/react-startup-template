ed8f6fb825729daf9247dcaf452d15aeede16e51
Iheb Chebbi <141638922+ihebchebbi1998tn@users.noreply.github.com>
2024-12-21 08:46:00 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,30 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { CartProvider } from './components/cart/CartProvider';
-import Index from './pages/Index';
-import CartPage from './pages/CartPage';
-import './App.css';
+import React from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Index from "./pages/Index";
+import CategoryPage from "./pages/CategoryPage";
 
-function App() {
-  return (
-    <Router>
-      <CartProvider>
+// Create a new QueryClient instance
+const queryClient = new QueryClient();
+
+const App = () => (
+  // Wrapping the entire app with QueryClientProvider to provide the query client context
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/cart" element={<CartPage />} />
+          <Route path="/category/:category" element={<CategoryPage />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
-      </CartProvider>
-    </Router>
-  );
-}
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
 
 export default App;
